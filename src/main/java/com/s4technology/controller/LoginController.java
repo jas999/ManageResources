0a0f2974d89bab5050f2a1b540adc42eeec7da54
Jaspreet Singh <jaspreet605@gmail.com>
2018-07-16 20:15:25 +0530

    Added logger

--- src/main/java/com/s4technology/controller/LoginController.java
+++ src/main/java/com/s4technology/controller/LoginController.java
@@ -1,5 +1,8 @@
 package com.s4technology.controller;
 
+
+import java.util.logging.Logger;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,12 +17,15 @@ import com.s4technology.services.UserInterface;
 @RestController
 public class LoginController {
 
+	static Logger perfLog = Logger.getLogger("LoginController");
+	
 	@Autowired
 	UserInterface userService;
 
+	
 	@RequestMapping(path = "/user/login")
 	public @ResponseBody JSONResultObject authenticateUser(@RequestBody User user) {
-
+		perfLog.info("This is apache log");
 		userService.authenticateUser(user);
 		JSONResultObject obj = new JSONResultObject();
 		obj.setResult("true");
